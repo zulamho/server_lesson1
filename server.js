fffa5209aa083fbdc6d4fdca76fd62e48bad421d
Амхадов Адам <teacher26@ithub.local>
2026-09-21 13:49:28 +0300

    cерверер с  библиотекой Express

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const http = require("http")// подключаем модуль http
+const express = require("express")// подключаем библиотеку express
 
+const app = express()// запускаем express
 const PORT = 3000;// порт на котор будем запускать сервер
 
-const server = http.createServer((req, res) => { // создаем сервер
-    res.end("Hello World") // ответ клиенту когда он зайдет на наш сервер
 
-})
+app.get('/' , (req , res) => {
+    res.send("Главная страница")
+} )
 
-server.listen(PORT, () => { // тут расписываем на каком порту запускается сервер
+
+
+// const server = http.createServer((req, res) => { // создаем сервер
+//     res.end("Hello World") // ответ клиенту когда он зайдет на наш сервер
+
+// })
+
+app.listen(PORT, () => { // тут расписываем на каком порту запускается сервер
     console.log("Сервер запущен успешно по адресу: 127.0.0.1:3000");// тут делаем уведомление для себя что сервер запущен
 
 })
